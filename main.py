09fcc76988ecd9c1dd091a11ec541b225575f461
Dig_kmen <113637671+Anne-LaureS@users.noreply.github.com>
2026-01-14 22:55:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ def start_server():
     print(f"[INFO] Démarrage du serveur sur {HOST}:{PORT}")
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
     server_socket.listen()
 
